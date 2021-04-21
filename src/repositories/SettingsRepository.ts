a1eba9b0f2f87fdcc4440d76add35bf5ffc6fa15
jessiLeandro <jessi_leandro@hotmail.com>
2021-04-21 14:25:36 -0300

    second day

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from "typeorm";
+
+import { Settings } from "../entities/Settings";
+
+@EntityRepository(Settings)
+class SettingsRepository extends Repository<Settings> {
+
+}
+
+export { SettingsRepository }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 
+import './database'
+import { routes } from './router' 
+
 /**
  * GET = buscas
  * POST = Criação
@@ -10,16 +13,20 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'OLÁ NLW 05!'
-  })
-})
+// app.get('/', (request, response) => {
+//   return response.json({
+//     message: 'OLÁ NLW 05!'
+//   })
+// })
 
-app.post('/', (request, response) => {
-  return response.json({
-    message: 'Usuário salvo com sucesso!'
-  })
-})
+// app.post('/', (request, response) => {
+//   return response.json({
+//     message: 'Usuário salvo com sucesso!'
+//   })
+// })
+
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333, () => console.log('server ir running on port 33333'))
